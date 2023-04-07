9c678e26d0f1db39b338d3932dd6d2a3e8b9d9bc
locdacknownothing <98312612+locdacknownothing@users.noreply.github.com>
2023-04-07 13:20:24 +0700

    add polyfills.ts

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import 'react-native-gesture-handler'
-import React from 'react'
+import './polyfills';
+// import 'react-native-gesture-handler'
 import { Provider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { theme } from './src/core/theme'
 import AuthStackNavigator from './src/navigators/AuthStackNavigator'
+import DrawerNavigator from './src/navigators/DrawerNavigator'
 
 export default function App() {
   return (
