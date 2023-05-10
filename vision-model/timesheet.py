8990980d2e5d654f72ff49a82f4b674b0003cd9c
loclt <loc.le1011@hcmut.edu.vn>
2023-05-10 15:16:40 +0700

    Add vision model

---
+++ vision-model/timesheet.py
@@ -0,0 +1,19 @@
+import requests
+from collections import defaultdict
+from datetime import datetime
+
+SERVER_URL = 'http://127.0.0.1:5000/attcheck/'
+
+previous_check = defaultdict(int)
+for data in requests.get(url=SERVER_URL+'all').json():
+    t = previous_check[data['id']], data['check_in']
+    if data['check_out']:
+        t += (data['check_out'],)
+    previous_check[data['id']] = max(t)
+
+
+def check(emp_id):
+    current = datetime.now().timestamp()
+    if current - previous_check[emp_id] > 300:
+        previous_check[emp_id] = current
+        requests.post(url=SERVER_URL, data={'id': emp_id})
